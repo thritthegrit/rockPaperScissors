7f3283723e0d0cb795671bf615b8b401eeebd3c5
Thrity Golzari <154635854+thritthegrit@users.noreply.github.com>
2024-07-10 15:14:56 -0700

    Added a playRound function
    This function goes over all the possible combinations of rock paper scissors and increments for one or both player and computer.

--- javascript.js
+++ javascript.js
@@ -13,6 +13,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     valid = false;
     humanChoice = prompt('Choose one between rock, paper, or scissors');
+    humanChoice = humanChoice.toLowerCase;
     while(!valid) {
         if (humanChoice === "rock") {
             valid = true;
@@ -27,4 +28,39 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice==='rock' && computerChoice ==='paper'){
+        console.log('You lose! Paper beats rock!');
+        computerScore++;
+    } else if (humanChoice==='rock' && computerChoice ==='scissors') {
+        console.log('You win! Rock beats scissors!');
+        humanScore++;
+    } else if (humanChoice==='rock' && computerChoice==='rock') {
+        console.log("Tie! Rock and Rock");
+        humanScore++;
+        computerScore++;
+    } else if (humanChoice==='paper' && computerChoice==='rock') {
+        console.log('You win! Paper beats rock!');
+        humanScore++;
+    } else if (humanChoice==='paper' && computerChoice==='scissors') {
+        console.log('You lose! Scissors beats paper')
+        computerScore++;
+    } else if (humanChoice==='paper' && computerChoice==='paper') {
+        console.log("Tie! Paper and Paper");
+        humanScore++;
+        computerScore++;
+    } else if (humanChoice==='scissors' && computerChoice==='paper') {
+        console.log('You win! Scissors beats paper');
+        humanScore++;
+    } else if (humanChoice==='scissors' && computerChoice==='rock') {
+        console.log('You lose! Rock beats scissors');
+        computerScore++;
+    } else if (humanChoice==='scissors' && computerChoice==='scissors') {
+        console.log('Tie! Scissors and Scissors');
+        humanScore++;
+        computerScore++;
+    }
+}
